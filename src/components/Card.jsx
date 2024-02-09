bb0f485c9892817ba9533694a09fc2041cee82d8
bhaveshittadwar <bitsadwar@gmail.com>
2024-02-09 01:46:33 -0500

    fix: cta color

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({title, description, projectImage, link}) => {
   return (
-    <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="w-full max-w-sm bg-slate-300 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <a href="#">
         <img className="rounded-t-lg" src={projectImage} alt="" />
       </a>
@@ -11,7 +11,7 @@ const Card = ({title, description, projectImage, link}) => {
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
         </a>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{description}</p>
-        <a href={link} target='_blank' className="w-fit mt-auto inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <a href={link} target='_blank' className="w-fit mt-auto inline-flex items-center px-3 py-2 text-sm font-medium text-center text-gray-950 bg-blue-700 rounded-lg hover:text-white hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-slate-200 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
           Let's Go
           <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
